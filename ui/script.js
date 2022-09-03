1f1388348e309a915851c5f937ad16e4ec48004f
ricardoinaciopt <ricardo.inacio.2002@gmail.com>
2022-09-03 10:05:17 +0100

    initial commit

---
+++ ui/script.js
@@ -0,0 +1,13 @@
+setTimeout(function () {
+  document.getElementById("intro2").classList.add("fade-in");
+  document.getElementById("intro2").style.opacity = 1;
+}, 5000);
+
+setTimeout(function () {
+  document.getElementById("intro3").classList.add("fade-in");
+  document.getElementById("intro3").style.opacity = 1;
+}, 8000);
+
+setTimeout(function () {
+  document.getElementById("face").src = "assets/images/face2.png";
+}, 10000);
