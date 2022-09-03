e7aabfa9e383e1c5b8e177ab0a0f8a5805e06b55
ricardoinaciopt <ricardo.inacio.2002@gmail.com>
2022-09-03 16:51:48 +0100

    intro screen finished

--- ui/script.js
+++ ui/script.js
@@ -6,8 +6,22 @@ setTimeout(function () {
 setTimeout(function () {
   document.getElementById("intro3").classList.add("fade-in");
   document.getElementById("intro3").style.opacity = 1;
+  document.getElementById("face").classList.add("fade-out");
+  document.getElementById("face").style.opacity = 0;
 }, 8000);
 
 setTimeout(function () {
-  document.getElementById("face").src = "assets/images/face2.png";
+  document.getElementById("face2").classList.add("fade-in-face");
+  document.getElementById("face2").style.opacity = 1;
+  document.getElementById("name-input").classList.add("fade-in");
+  document.getElementById("name-input").style.opacity = 1;
+  document.getElementById("ok-input").classList.add("fade-in");
+  document.getElementById("ok-input").style.opacity = 1;
 }, 10000);
+
+function getName() {
+  let name = document.getElementById("name-input").value;
+  if (name !== null && name !== "") {
+    location.replace("main.html");
+  }
+}
