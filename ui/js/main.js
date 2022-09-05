a4145af4f86be5d6282be6ac4fe9f466385df200
ricardoinaciopt <ricardo.inacio.2002@gmail.com>
2022-09-05 16:39:59 +0100

    user settings progress

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,22 +1,43 @@
-const { writeTextFile, readTextFile, BaseDirectory } = window.__TAURI__.fs;
+const { writeTextFile, readTextFile, readDir, BaseDirectory } =
+  window.__TAURI__.fs;
 
-if (document.getElementById("ok-input") !== null) {
-  document.getElementById("ok-input").addEventListener(
-    "click",
-    async function () {
-      let name = document.getElementById("name-input").value;
-      if (name !== null && name !== "") {
-        await writeTextFile(
-          { path: "user.conf", contents: name },
-          { dir: BaseDirectory.App }
-        );
-        location.replace("main.html");
-      }
-    },
-    false
-  );
-}
+const entries = await readDir("", {
+  dir: BaseDirectory.App,
+  recursive: true,
+});
 
-if (document.getElementById("user") !== null) {
-  const username = await readTextFile("user.conf", { dir: BaseDirectory.App });
+const filesInDir = Array.from(entries);
+
+console.log(entries);
+console.log("-------------");
+console.log(filesInDir.length);
+
+switch (filesInDir.lenght) {
+  case 0:
+    console.log("nao tem");
+    if (document.getElementById("ok-input") !== null) {
+      document
+        .getElementById("ok-input")
+        .addEventListener("click", async function () {
+          let name = document.getElementById("name-input").value;
+          if (name !== null && name !== "") {
+            writeTextFile(
+              { path: "user.conf", contents: name },
+              { dir: BaseDirectory.App }
+            );
+          }
+        });
+      location.replace("main.html");
+    }
+    break;
+  default:
+    console.log("tem file");
+    const username = readTextFile("user.conf", {
+      dir: BaseDirectory.App,
+    });
+    location.replace("main.html");
+    if (document.getElementById("user") !== null) {
+      document.getElementById("user").textContent = username;
+    }
+    break;
 }
